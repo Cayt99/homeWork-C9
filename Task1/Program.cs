5dd866bbf2253c07b2c80f3f59185f4331588eb6
Saut <cayt99@mail.ru>
2023-07-21 18:03:02 +0600

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N < 1)
+        {
+            Console.WriteLine("Введено некорректное значение. Пожалуйста, введите другое число.");
+            return;
+        }
+
+        Console.Write($"N = {N} -> \"");
+        PrintNaturalNumbersInReverse(N);
+        Console.WriteLine("\"");
+    }
+
+    static void PrintNaturalNumbersInReverse(int N)
+    {
+        if (N < 1)
+        {
+            return;
+        }
+
+        Console.Write(N);
+
+        if (N > 1)
+        {
+            Console.Write(", ");
+            PrintNaturalNumbersInReverse(N - 1);
+        }
+    }
+}
+
